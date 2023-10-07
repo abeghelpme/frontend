6c0774d9e28bcd2ba6e4636c27dcb089ea66345d
Klin-Coderse <obcbeats@gmail.com>
2023-10-07 23:54:59 +0300

    linter

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  '*.{js,jsx,ts,tsx}': ['eslint --fix', 'eslint'],
+  '*.{js,jsx,ts,tsx}': ['eslint .', 'eslint'],
   '**/*.ts?(x)': () => 'npm run check-types',
   '*.{json,yaml}': ['prettier --write'],
 };
