196bc9c65ef9cde9a86ee25ed1ed9b17358da29b
Klin-Coderse <obcbeats@gmail.com>
2023-10-09 20:51:57 +0300

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,8 @@ export default function Home() {
   return (
     <div>
       <h1>WELCOME TO ABEG HELP</h1>
+      <h1>WELCOME TO ABEG HELP</h1>
+      <h1>WELCOME TO ABEG HELP</h1>
     </div>
   );
 }
