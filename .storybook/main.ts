2f5d7fab8f36855573db56b8b5939807787a297f
Olabode Lawal-Shittabey <babblebey@gmail.com>
2023-10-16 21:09:25 +0100

    feat: add `storybookjs` to project (#70)
    * chore: remove `src` dir
    
    * chore(build): auto lints
    
    * chore: add `storybook` and assoc addons
    
    * feat: configure stories preview enviroment
    
    * feat: boilerplate example stories
    
    * chore: update documentation

---
+++ .storybook/main.ts
@@ -0,0 +1,29 @@
+import type { StorybookConfig } from '@storybook/nextjs';
+
+const config: StorybookConfig = {
+  stories: [
+    '../stories/**/*.stories.mdx',
+    '../stories/**/*.stories.@(js|jsx|ts|tsx)',
+    '../stories/**/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-onboarding',
+    '@storybook/addon-interactions',
+    {
+      name: '@storybook/addon-styling',
+      options: {
+        postCss: true,
+      },
+    },
+  ],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
+  },
+  docs: {
+    autodocs: true,
+  },
+};
+export default config;
--- README.md
+++ README.md
@@ -1,10 +1,36 @@
-# abeghelp-frontend
+<div align="center">
+  <br>
+  <h1>🤲 AbegHelp 🤲</h1>
+  <p><i>"Abeg, a man of many talents and skills. He is the best in all things."</i></p>
+</div>
+<br>
+
+## Local development
+
+Install application dependencies
+
+```shell 
+npm install
+````
+Run Husky
+
+```shell
+npm run prepare
+```
+Start a local copy of the app on port `3000`
+
+```shell
+npm run dev
+```
+## Storybook
+
+The Storybook for this project can be started and viewed locally on port `6006`
+
+```shell
+npm run storybook
+```
 
 
-# Install dependencies and run husky
-
-run ```npm install```
-run ```npx husky install```
 
 ## Collaborators
 
