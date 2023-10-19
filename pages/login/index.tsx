d218d101ed2c41d6a9e69dbc5e61dfe5b83b5575
Jemimah Otebele <flashyjotebele@gmail.com>
2023-10-19 12:09:52 +0100

    feat:added boilerplate for login

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -3,34 +3,36 @@ import Link from 'next/link';
 
 const Login = () => {
   return (
-    <main className="min-h-screen w-full flex justify-center items-center p-4">
-      <div className="p-8 bg-blue-950 rounded-lg w-full max-w-sm space-y-5">
+    <main className="flex items-center justify-center w-full min-h-screen p-4">
+      <div className="w-full max-w-sm p-8 space-y-5 rounded-lg bg-blue-950">
         <h1 className="text-xl font-bold">Sign in to our platform</h1>
-        <form className="flex flex-col gap-7">
+        <form className="flex flex-col gap-5">
           <div className="flex flex-col gap-2">
-            <label htmlFor="email" className="font-medium text-sm">
+            <label htmlFor="email" className="text-sm font-medium">
               Your Email
             </label>
             <input
               type="email"
               className="py-2 rounded-lg bg-[#4B5563] text-gray-400 focus:outline-none px-4  border border-gray-400"
               id="email"
+              placeholder="name@gmail.com"
               required
             />
           </div>
           <div className="flex flex-col gap-2">
-            <label htmlFor="password" className="font-medium text-sm">
+            <label htmlFor="password" className="text-sm font-medium">
               Your Password
             </label>
             <input
               type="password"
+              placeholder="......."
               className="py-2 rounded-lg bg-[#4B5563] text-gray-400 focus:outline-none px-4  border border-gray-400"
               id="password"
               required
             />
           </div>
-          <div className="flex justify-between   gap-2">
-            <span className="flex gap-2 items-center">
+          <div className="flex justify-between gap-2">
+            <span className="flex items-center gap-2">
               <input type="checkbox" id="remember-me" />
               <label className="" htmlFor="remember-me">
                 Remember me
