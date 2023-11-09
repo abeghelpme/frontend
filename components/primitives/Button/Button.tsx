8e2df242299b64c0d0719bd6d611506d0faafd3e
Jemimah Otebele <flashyjotebele@gmail.com>
2023-11-09 15:00:38 +0100

    fix:added functionality to button variant and size

--- components/primitives/Button/Button.tsx
+++ components/primitives/Button/Button.tsx
@@ -1,21 +1,47 @@
+import { cn } from "@/lib/utils/cn";
 import React, { forwardRef } from "react";
 
 interface ButtonProps extends React.ComponentPropsWithRef<"button"> {
   className: string;
   disabled?: boolean;
+  fullWidth?: boolean;
   children: React.ReactNode;
   size?: "sm" | "base" | "lg";
   variant?: "primary" | "secondary" | "danger";
 }
 
-// const buttonClass = cl
-
 const Button = (
-  { className, children, ...props }: ButtonProps,
+  {
+    className,
+    children,
+    fullWidth,
+    variant,
+    size,
+    disabled,
+    ...props
+  }: ButtonProps,
   ref: React.ForwardedRef<HTMLButtonElement>,
 ) => {
+  const buttonClass = {
+    "bg-abeg-error-20": variant === "danger",
+    "border-abeg-green text-abeg-green border": variant === "secondary",
+    "bg-abeg-neutral-20": variant === "primary",
+    "px-3": size === "sm",
+    "px-10": size === "lg",
+    "w-full": fullWidth,
+    "bg-abeg-neutral-50 text-white": disabled,
+  };
+
   return (
-    <button className={className} ref={ref} {...props}>
+    <button
+      className={cn(
+        "px-5 py-2 rounded-lg text-white text-sm font-medium",
+        buttonClass,
+        className,
+      )}
+      ref={ref}
+      {...props}
+    >
       {children}
     </button>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,7 @@
-// import Button from "@/components/primitives/Button/Button";
-
 export default function Home() {
   return (
     <div>
       <h1>WELCOME TO ABEG HELP!!</h1>
-      {/* uncomment to see the button in action */}
-      {/* <Button
-        onClick={() => console.log("clicked")}
-        className="bg-red-500 rounded-lg px-7 py-2 "
-        disabled={false}
-        size="sm"
-        variant="primary"
-      >
-        Click me
-      </Button> */}
     </div>
   );
 }
