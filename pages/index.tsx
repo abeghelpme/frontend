b782c309a1d049bed7179a84ebf91113fba76310
Olamide <onabiyiolamide13@gmail.com>
2023-11-20 14:14:40 +0100

    Improved accessibilty

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,7 @@
-import Input from "@/components/primitives/Form/Input";
-import { useRef } from "react";
-
 export default function Home() {
-  const ref = useRef<HTMLInputElement | null>(null);
-
   return (
-    <div className={"px-4"}>
+    <div>
       <h1>WELCOME TO ABEG HELP!!</h1>
-      <Input ref={ref} placeholder={"Enter Email"} type={"password"} />
     </div>
   );
 }
