c963b2399963ae93b698bd603ce81c2eb676bc48
Ryan Zayne <93886198+Ryan-Zayne@users.noreply.github.com>
2023-11-22 11:27:42 +0100

    Update createFetcherInstance.ts

--- lib/utils/createFetcherInstance.ts
+++ lib/utils/createFetcherInstance.ts
@@ -3,16 +3,12 @@ type BaseFetcherOptions<TString extends string> = RequestInit & {
   baseTimeout?: number;
 };
 
-type FetcherOptions = RequestInit & {
-  timeout?: number;
-};
+type FetcherOptions = RequestInit & { timeout?: number; };
 
 const createFetcherInstance = <TBaseURL extends string>(
   baseOptions: BaseFetcherOptions<TBaseURL>,
 ) => {
   const { baseURL, baseTimeout = 10000, ...restOfBaseOptions } = baseOptions;
-
-  const baseController = new AbortController();
 
   const fetcherFn = async <TResponseData>(
     url: `/${string}`,
@@ -25,8 +21,7 @@ const createFetcherInstance = <TBaseURL extends string>(
 
     try {
       const response = await fetch(`${baseURL}${url}`, {
-        signal:
-          timeout === baseTimeout ? baseController.signal : controller.signal,
+        signal: controller.signal,
         ...restOfBaseOptions,
         ...restOfFetchOptions,
       });
@@ -39,7 +34,7 @@ const createFetcherInstance = <TBaseURL extends string>(
 
       return response.json() as TResponseData;
 
-      // Re-throw error in case of timeout for better debugging
+      // Re-throw error with helpful message in case of timeout
     } catch (error) {
       if (error instanceof DOMException && error.name === "AbortError") {
         throw new Error(`Request to ${url} timed out after ${timeout}ms`);
@@ -48,8 +43,8 @@ const createFetcherInstance = <TBaseURL extends string>(
   };
 
   fetcherFn.defaults = baseOptions; /* REVIEW - This is up for debate,
-		 														whether or not we want to be able to override the defaults from instance just like axios
-												*/
+	
+		                      whether or not we want to be able to override the defaults from instance just like axios */									*/
 
   return fetcherFn;
 };
