7b48cd039614ca533dc38a11d7b48202ad1a374a
babblebey <babblebey@gmail.com>
2023-11-22 03:55:34 +0100

    feat: configure `_app` ready for `layouts`; add interface for page integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,23 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import type { ComponentType, ReactNode } from "react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+interface ComponentWithPageLayout extends AppProps {
+  Component: AppProps["Component"] & {
+    PageLayout?: ComponentType<{ children: ReactNode }>;
+  };
+}
+
+export default function App({ Component, pageProps }: ComponentWithPageLayout) {
+  return (
+    <>
+      {Component.PageLayout ? (
+        <Component.PageLayout>
+          <Component {...pageProps} />
+        </Component.PageLayout>
+      ) : (
+        <Component {...pageProps} />
+      )}
+    </>
+  );
 }
