03dbb5339e3add7dc0cc80a6387e1acf817cffeb
Ryan-Zayne <phoenixastro97@gmail.com>
2023-11-22 01:45:13 +0100

    reverts

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,10 +1,8 @@
 /** @type {import("prettier").Config} */
 
 module.exports = {
-	semi: true,
-	trailingComma: "all",
-	singleQuote: false,
-	jsxSingleQuote: false,
-	useTabs: true,
-	printWidth: 100,
+  semi: true,
+  trailingComma: "all",
+  singleQuote: false,
+  jsxSingleQuote: false,
 };
