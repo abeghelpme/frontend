81cd9c2d2e14718e775b198283865a022b59722e
Ryan-Zayne <phoenixastro97@gmail.com>
2023-11-22 01:48:18 +0100

    ...Draft

--- lib/utils/createFetcherInstance.ts
+++ lib/utils/createFetcherInstance.ts
@@ -1,53 +1,57 @@
 type BaseFetcherOptions<TString extends string> = RequestInit & {
-	baseURL: TString;
-	baseTimeout?: number;
+  baseURL: TString;
+  baseTimeout?: number;
 };
 
 type FetcherOptions = RequestInit & {
-	timeout?: number;
+  timeout?: number;
 };
 
 const createFetcherInstance = <TBaseURL extends string>(
-	baseOptions: BaseFetcherOptions<TBaseURL>,
+  baseOptions: BaseFetcherOptions<TBaseURL>,
 ) => {
-	const { baseURL, baseTimeout = 10000, ...restOfBaseOptions } = baseOptions;
+  const { baseURL, baseTimeout = 10000, ...restOfBaseOptions } = baseOptions;
 
-	const baseController = new AbortController();
+  const baseController = new AbortController();
 
-	const fetcherFn = async <TResponseData>(url: `/${string}`, options: FetcherOptions = {}) => {
-		const { timeout = baseTimeout, ...restOfFetchOptions } = options;
+  const fetcherFn = async <TResponseData>(
+    url: `/${string}`,
+    options: FetcherOptions = {},
+  ) => {
+    const { timeout = baseTimeout, ...restOfFetchOptions } = options;
 
-		const controller = new AbortController();
-		const ID = window.setTimeout(() => controller.abort(), timeout);
+    const controller = new AbortController();
+    const ID = window.setTimeout(() => controller.abort(), timeout);
 
-		try {
-			const response = await fetch(`${baseURL}${url}`, {
-				signal: timeout === baseTimeout ? baseController.signal : controller.signal,
-				...restOfBaseOptions,
-				...restOfFetchOptions,
-			});
+    try {
+      const response = await fetch(`${baseURL}${url}`, {
+        signal:
+          timeout === baseTimeout ? baseController.signal : controller.signal,
+        ...restOfBaseOptions,
+        ...restOfFetchOptions,
+      });
 
-			window.clearTimeout(ID);
+      window.clearTimeout(ID);
 
-			if (!response.ok) {
-				throw new Error("Failed to fetch data from server");
-			}
+      if (!response.ok) {
+        throw new Error("Failed to fetch data from server");
+      }
 
-			return response.json() as TResponseData;
+      return response.json() as TResponseData;
 
-			// Re-throw error in case of timeout for better debugging
-		} catch (error) {
-			if (error instanceof DOMException && error.name === "AbortError") {
-				throw new Error(`Request to ${url} timed out after ${timeout}ms`);
-			}
-		}
-	};
+      // Re-throw error in case of timeout for better debugging
+    } catch (error) {
+      if (error instanceof DOMException && error.name === "AbortError") {
+        throw new Error(`Request to ${url} timed out after ${timeout}ms`);
+      }
+    }
+  };
 
-	fetcherFn.defualts = baseOptions; /* REVIEW - This is up for debate,
+  fetcherFn.defualts = baseOptions; /* REVIEW - This is up for debate,
 		 														whether or not we want to be able to override the defaults from instance just like axios
 												*/
 
-	return fetcherFn;
+  return fetcherFn;
 };
 
 export { createFetcherInstance };
--- lib/utils/fetcher.ts
+++ lib/utils/fetcher.ts
@@ -2,15 +2,15 @@ import { createFetcherInstance } from "./createFetcherInstance";
 
 // Added this type guard to assert that baseURL is a string, as opposed unsafe typecasting via "as string"
 const assertBaseURL = (baseURL: string | undefined) => {
-	if (typeof baseURL !== "string") {
-		throw new Error("baseURL must be a string");
-	}
+  if (typeof baseURL !== "string") {
+    throw new Error("baseURL must be a string");
+  }
 
-	return baseURL;
+  return baseURL;
 };
 
 const fetcherFn = createFetcherInstance({
-	baseURL: assertBaseURL(process.env.NEXT_PUBLIC_BACKEND_URL),
+  baseURL: assertBaseURL(process.env.NEXT_PUBLIC_BACKEND_URL),
 });
 
 export { fetcherFn };
