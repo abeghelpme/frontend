0fca392d364a497df2f91b65fc5596f516d2aba8
Sosaristic <sundayomena9@gmail.com>
2023-11-30 06:42:04 +0100

    fix: 404 page issued fixed

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,24 +1,26 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import Button from "@/components/primitives/Button/button";
+
 const NotFound = () => {
   return (
-    <main className=" min-h-screen flex px-4 flex-col-reverse justify-center bg-abeg-neutral-60 lg:flex-row">
-      <section className="relative flex-1 flex flex-col gap-4 items-center justify-start lg:justify-center lg:w-1/2">
+    <main className=" min-h-screen flex px-4 flex-col-reverse justify-center items-center bg-abeg-neutral-60 lg:flex-row">
+      <section className="relative  flex flex-col gap-4 items-center justify-start lg:justify-center lg:w-1/2">
         <p className="text-2xl font-bold">Oooops!</p>
         <p className="text-lg font-semibold text-center">
           We can&apos;t seem to find the page you are looking for
         </p>
         <Link href="/">
-          <button className="bg-abeg-green h-[3rem] focus:outline-none focus:ring  focus:ring-abeg-neutral-70 px-4 py-[0.875rem]  flex items-center justify-center rounded-[0.5rem] text-abeg-neutral-70">
+          <Button className="" variant="primary">
             Go Home
-          </button>
+          </Button>
         </Link>
       </section>
-      <section className="relative flex-1 lg:w-1/2">
+      <section className="relative flex items-center justify-center h-[15rem] md:h-[30rem] w-full lg:w-1/2">
         <Image
           src="/404.webp"
-          alt="404 page image illustration"
+          alt=""
           sizes="(min-width: 808px) 50vw, 100vw"
           priority
           fill
