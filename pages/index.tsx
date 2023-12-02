cf55929cb9c5a9d32b556f98d9efd3d8c625d90d
Olabode Lawal-Shittabey <babblebey@gmail.com>
2023-12-02 01:21:26 +0100

    feat: add page layout setup (#129)
    * feat: boilerplate `footer` and `navbar` components
    
    * feat: boilerplate `base-layout`
    
    * feat: configure `_app` ready for `layouts`; add interface for page integration
    
    * feat: integrate in `index` page
    
    * chore: add `ReactQueryDevtools` missing from resolved conflict

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,14 @@
-export default function Home() {
+import BaseLayout from "@/layouts/base-layout";
+import type { WithPageLayout } from "@/interfaces/with-page-layout";
+
+const Home: WithPageLayout = () => {
   return (
     <div>
       <h1>WELCOME TO ABEG HELP!!</h1>
     </div>
   );
-}
+};
+
+Home.PageLayout = BaseLayout;
+
+export default Home;
