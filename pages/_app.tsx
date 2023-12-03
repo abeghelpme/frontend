23cf84f872b97c5baf3e371e2eb31da1e04f8c70
Kelechukwu <paparazzip8@gmail.com>
2023-12-03 05:58:28 +0100

    added nprogress for loading

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,15 @@ import "@/styles/globals.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import type { AppProps } from "next/app";
+import "../styles/nprogress.css";
+import Router from "next/router";
+import nProgress from "nprogress";
 
 const queryClient = new QueryClient();
+
+Router.events.on("routeChangeStart", () => nProgress.start());
+Router.events.on("routeChangeError", () => nProgress.done());
+Router.events.on("routeChangeComplete", () => nProgress.done());
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
