9c9bf9ad7a3b35f8d1ac5f4022348f56408cc7c7
Kelechukwu <paparazzip8@gmail.com>
2023-12-05 18:52:13 +0100

    added next-nprogress-bar package for loading state

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import type { AppProps } from "next/app";
 import type { ComponentType, ReactNode } from "react";
-
+import { PagesProgressBar as ProgressBar } from "next-nprogress-bar";
 interface ComponentWithPageLayout extends AppProps {
   Component: AppProps["Component"] & {
     PageLayout?: ComponentType<{ children: ReactNode }>;
@@ -16,6 +16,12 @@ export default function App({ Component, pageProps }: ComponentWithPageLayout) {
   return (
     <>
       <QueryClientProvider client={queryClient}>
+        <ProgressBar
+          height="3px"
+          color="green"
+          options={{ showSpinner: false }}
+          shallowRouting
+        />
         {Component.PageLayout ? (
           <Component.PageLayout>
             <Component {...pageProps} />
