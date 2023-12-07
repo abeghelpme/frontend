4c4f2ffc8c84e1721023e5ae84efd94d9a0bca41
Roland Joshua <289volts@gmail.com>
2023-12-07 16:37:40 +0100

    Feat implement signup (#136)
    * .
    
    * Feat
    
    Added custom zod validation helper function and updated sign up prop types
    
    * Feat
    
    Added logoBanner component and edited stylesheet to remove background
    
    * Feat
    
    Signup UI done with validations in place. Remaining sending of data to the backend
    
    * Feat
    
    Added verification and success pages. Modified zod validator to also validate for other auth pages. Modified stylesheet so as to help position layout in a more responsive way

---
+++ layouts/logoBanner.tsx
@@ -0,0 +1,21 @@
+import React, { type FC } from "react";
+import logo from "@/public/assets/images/shared/logo.svg";
+import Image from "next/image";
+
+type CompProp = {
+  textColor: string;
+};
+const LogoBanner: FC<CompProp> = ({ textColor }) => {
+  return (
+    <div className="flex items-center gap-2 justify-center">
+      <Image
+        className="lg:w-[66px] lg:aspect-square"
+        src={logo as string}
+        alt="AbegHelp"
+      />
+      <span className={`text-${textColor} font-medium`}>AbegHelp.me</span>
+    </div>
+  );
+};
+
+export default LogoBanner;
