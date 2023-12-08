be5bf03360dc0858cf85ddb8396c839dd7a05b8a
Kelechukwu <paparazzip8@gmail.com>
2023-12-08 01:46:38 +0100

    feat: resolved all PR reviews

--- pages/reset-password/confirmation.tsx
+++ pages/reset-password/confirmation.tsx
@@ -1,38 +1,21 @@
 import React from "react";
 import Link from "next/link";
-import { AbegHelpMe } from "@/components/primitives/abeg-help.me/abeg-help-me-svg";
+import { Player } from "@lottiefiles/react-lottie-player";
+import LogoBanner from "@/layouts/logoBanner";
 
 export default function Confirmation() {
   return (
     <main className="px-5 h-[100dvh] w-full">
-      <div className="pt-10 flex justify-center mb-40">
-        <AbegHelpMe />
-      </div>
+      <LogoBanner textColor="formTemp" />
       <div className="mx-auto w-full bg-white p-6 rounded-md justify-center items-center md:w-1/2 lg:w-4/12 space-y-10">
         <div>
           <div className="flex justify-center items-center mb-8">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="120"
-              height="120"
-              viewBox="0 0 120 120"
-              fill="none"
-            >
-              <path
-                d="M80.625 48.75L53.1094 75L39.375 61.875"
-                stroke="#007004"
-                strokeWidth="5"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-              <path
-                d="M60 105C84.8528 105 105 84.8528 105 60C105 35.1472 84.8528 15 60 15C35.1472 15 15 35.1472 15 60C15 84.8528 35.1472 105 60 105Z"
-                stroke="#007004"
-                strokeWidth="5"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
+            <Player
+              autoplay
+              loop
+              src="https://lottie.host/a60494de-7c09-4dbd-b016-97035289ba6a/4FRd7uct0G.json"
+              style={{ height: "200px", width: "200px" }}
+            />
           </div>
           <div className="flex justify-center">
             <h1 className="text-2xl font-semibold text-gray-900">Success!</h1>
@@ -43,7 +26,7 @@ export default function Confirmation() {
         </div>
         <div>
           <Link
-            href={"signin"}
+            href={"/signin"}
             className="bg-abeg-button-10 hover:bg-abeg-button-20 transition duration-300 text-center py-3 text-white font-medium rounded-md w-full"
           >
             <button className="w-full" type="button">
--- pages/reset-password/index.tsx
+++ pages/reset-password/index.tsx
@@ -1,23 +1,24 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useDeferredValue } from "react";
 import { useRouter } from "next/router";
 import type { NextRouter } from "next/router";
 import Input from "@/components/primitives/Form/Input";
 import Button from "@/components/primitives/Button/button";
 import { useForm } from "react-hook-form";
-import { z, ZodError, ZodType } from "zod";
+import { ZodError } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { AbegHelpMe } from "@/components/primitives/abeg-help.me/abeg-help-me-svg";
+import {
+  checkPasswordStrength,
+  zodValidator,
+  type ResetPasswordType,
+} from "@/lib/utils/validation/validateWithZod";
+import LogoBanner from "@/layouts/logoBanner";
+import ProgressBar from "@/components/primitives/ProgressBar/progress-bar";
 // import *  from "../../public/abeg-auth-bg.png"
 
 // Define custom API error type
 type ApiErrorResponse = {
   status: "Error";
   message: string;
-};
-
-type FormValues = {
-  password: string;
-  confirmPassword: string;
 };
 
 type formData = {
@@ -30,27 +31,39 @@ type QueryParams = {
 };
 
 // Define Zod schema for password and confirmPassword validation
-const passwordSchema: ZodType<formData> = z
-  .object({
-    password: z
-      .string()
-      .regex(
-        /^(?=.*[!@#$%^&*(),.?":{}|<>])(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/,
-        {
-          message:
-            "Password must contain at least one special character, one digit, one lowercase letter, and one uppercase letter, and must not be less than 8 characters",
-        },
-      ),
-    confirmPassword: z.string(),
-  })
-  .refine((data) => data.password === data.confirmPassword, {
-    message: "Passwords do not match",
-    path: ["confirmPassword"],
-  });
+// const passwordSchema: ZodType<formData> = z
+//   .object({
+//     password: z
+//       .string()
+//       .min(6, { message: "Password must be at least 6 characters" })
+//       .max(50)
+//       .regex(
+//         /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[.,!@#$%^&*])[A-Za-z\d.,!@#$%^&*]{6,}$/,
+//         {
+//           message:
+//             "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character",
+//         },
+//       ),
+//     // .refine(
+//     //   async (value) => {
+//     //     const result = await checkPasswordStrength(value);
+//     //     return typeof result === "number" && result < 3 ? false : true;
+//     //   },
+//     //   {
+//     //     message: "Password is too weak!!",
+//     //   },
+//     // ),
+//     confirmPassword: z.string().min(6, { message: "Passwords must match" }),
+//   })
+//   .refine((data) => data.password === data.confirmPassword, {
+//     message: "Passwords do not match",
+//     path: ["confirmPassword"],
+//   });
 
 const ResetPassword: React.FC = () => {
   const router: NextRouter = useRouter();
   const [serverError, setServerError] = useState("");
+  // const password: string = watch("password", "");
   const { token }: QueryParams = router.query as QueryParams;
   const {
     handleSubmit,
@@ -58,12 +71,29 @@ const ResetPassword: React.FC = () => {
     formState: { errors: formErrors },
     setError,
     clearErrors,
-  } = useForm<FormValues>({
+    watch,
+  } = useForm<ResetPasswordType>({
+    //import and use resetPassword type here
+    resolver: zodResolver(zodValidator("resetPassword")!), //import and use resolver: zodResolver(zodValidator("resetPassword")!),
     mode: "onChange",
-    resolver: zodResolver(passwordSchema),
+    reValidateMode: "onChange",
   });
 
-  const onSubmit = async (data: FormValues) => {
+  const password: string = watch("password", "");
+  const [result, setResult] = useState<number>(0);
+  const deferredPassword = useDeferredValue(password);
+
+  useEffect(() => {
+    const genStrength = async () => {
+      const passwordStrength = await checkPasswordStrength(deferredPassword); //import and use checkPasswordStrength here
+      setResult(passwordStrength);
+    };
+    genStrength().catch((e) => {
+      console.log(e);
+    });
+  }, [deferredPassword]);
+
+  const onSubmit = async (data: formData) => {
     try {
       const response = await fetch(
         "https://abeghelp-backend-staging.up.railway.app/api/v1/auth/password/reset",
@@ -88,7 +118,7 @@ const ResetPassword: React.FC = () => {
         setServerError(responseData.message);
         // Redirect to another page after 2 seconds
         await new Promise<void>((resolve) => setTimeout(resolve, 2000));
-        await router.push("signin");
+        await router.push("/signin");
       }
     } catch (error) {
       if (error instanceof ZodError) {
@@ -122,11 +152,12 @@ const ResetPassword: React.FC = () => {
   ]);
 
   return (
-    <main className="px-5 h-[100dvh] w-full">
-      <div className="pt-10 flex justify-center mb-40">
-        <AbegHelpMe />
-      </div>
-      <div className="mx-auto w-full bg-white p-6 rounded-md justify-center items-center md:w-1/2 lg:w-4/12">
+    <div
+      role=""
+      className="mx-auto w-full bg-white p-6 rounded-md justify-center items-center md:w-1/2 lg:w-4/12"
+    >
+      <LogoBanner textColor="formTemp" />
+      <div className="space-y-6 w-full h-full lg:space-y-0 mt-40">
         <form
           onSubmit={(event) => {
             event.preventDefault();
@@ -161,6 +192,37 @@ const ResetPassword: React.FC = () => {
                     type="password"
                   />
                 </div>
+                {password.length > 0 && (
+                  <div>
+                    <ProgressBar
+                      value={result * 25}
+                      className={`${
+                        result < 2
+                          ? "progress-filled:bg-red-500"
+                          : result >= 2 && result <= 3
+                            ? "progress-filled:bg-yellow-500"
+                            : "progress-filled:bg-green-500"
+                      }`}
+                    />
+                    <p
+                      className={`${
+                        result <= 2
+                          ? "text-red-500"
+                          : result >= 2 && result <= 3
+                            ? "text-yellow-500"
+                            : "text-green-500"
+                      } text-sm`}
+                    >
+                      <span className="text-black">Password strength:</span>
+                      &nbsp;
+                      {result < 2
+                        ? "Weak"
+                        : result >= 2 && result <= 3
+                          ? "Medium"
+                          : "Strong"}
+                    </p>
+                  </div>
+                )}
                 {formErrors.password && (
                   <div className="text-red-500">
                     <h1>{formErrors.password.message}</h1>
@@ -194,7 +256,7 @@ const ResetPassword: React.FC = () => {
           </Button>
         </form>
       </div>
-    </main>
+    </div>
   );
 };
 export default ResetPassword;
