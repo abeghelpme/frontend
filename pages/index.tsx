7dbcff3e5b199d3fef3d8fe8fb848681bdc720ba
Roland Joshua <289volts@gmail.com>
2023-12-21 12:51:21 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,9 @@ import { socketHelper } from "@/lib/utils/socket-helper";
 import { useEffect } from "react";
 
 const Home: WithPageLayout = () => {
-  useEffect(() => {
-    socketHelper();
-  }, []);
+  // useEffect(() => {
+  //   socketHelper();
+  // }, []);
 
   return (
     <div>
