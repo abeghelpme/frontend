28415c74ea66eb5a60a90762d58597f546524f19
1finedev <obcbeats@gmail.com>
2024-01-22 16:55:12 +0300

    fix turnstile styling

--- components/CloudflareTurnstile/CloudFlareTurnStile.tsx
+++ components/CloudflareTurnstile/CloudFlareTurnStile.tsx
@@ -1,19 +1,21 @@
 import Script from "next/script";
 
+const SITEKEY = process.env.NEXT_PUBLIC_CF_TURNSTILE_SITEKEY as string;
 const CloudFlareTurnStile = () => {
+  if (!SITEKEY) {
+    throw new Error(
+      "Please add the NEXT_PUBLIC_CF_TURNSTILE_SITEKEY variable to your .env file",
+    );
+  }
   return (
-    <>
+    <div className="flex w-full flex-col items-center justify-center">
       <Script
         src="https://challenges.cloudflare.com/turnstile/v0/api.js"
         async
         defer
       />
-      <div
-        data-size="compact"
-        className="cf-turnstile [&_>_iframe]:!w-56 [&_>_iframe]:!h-24"
-        data-sitekey={process.env.NEXT_PUBLIC_CF_TURNSTILE_SITEKEY}
-      ></div>
-    </>
+      <div className="cf-turnstile mt-2" data-sitekey={SITEKEY}></div>
+    </div>
   );
 };
 
