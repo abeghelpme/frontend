34eeb5b26cd61a12948efe456b814e0f1470bf8f
masako29 <49831574+1finedev@users.noreply.github.com>
2024-02-07 17:28:36 +0300

    fix

--- components/Shared/CustomDialog.tsx
+++ components/Shared/CustomDialog.tsx
@@ -3,7 +3,7 @@ import {
 	DialogClose,
 	DialogContent,
 	DialogTrigger,
-} from "@/components/index";
+} from "@/components";
 import { CrossCircledIcon } from "@radix-ui/react-icons";
 import { type ReactNode, type SetStateAction } from "react";
 
