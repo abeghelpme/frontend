65290c396e0290571e0d6474c94d5025d74f2e93
Ryan Zayne <phoenixastro97@gmail.com>
2024-02-08 14:28:52 +0100

    .

--- components/ui/Toggle.tsx
+++ components/ui/Toggle.tsx
@@ -25,7 +25,9 @@ function Toggle(
 	);
 }
 
-export default forwardRef(Toggle);
+const ForwardedToggle = forwardRef(Toggle);
+
+export default ForwardedToggle;
 
 export const toggleVariants = cva(
 	"inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors hover:bg-muted hover:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=on]:bg-accent data-[state=on]:text-accent-foreground",
