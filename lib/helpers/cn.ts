13b160f9a174c8ca1f39c9184b3594f597e63f0b
Ryan Zayne <phoenixastro97@gmail.com>
2024-02-12 22:14:50 +0100

    .

--- lib/helpers/cn.ts
+++ lib/helpers/cn.ts
@@ -2,16 +2,16 @@ import twConfig from "@/tailwind.config";
 import clsx, { type ClassValue } from "clsx";
 import { extendTailwindMerge } from "tailwind-merge";
 
-const themeObject = twConfig.theme?.extend;
+const themeObject = twConfig.theme.extend;
 
 const customTwMerge = extendTailwindMerge({
 	extend: {
 		classGroups: {
-			"font-size": Object.keys(themeObject?.fontSize ?? []).map(
+			"font-size": Object.keys(themeObject.fontSize).map(
 				(key) => `text-${key}`
 			),
 
-			rounded: Object.keys(themeObject?.borderRadius ?? []).map(
+			rounded: Object.keys(themeObject.borderRadius).map(
 				(key) => `rounded-${key}`
 			),
 		},
