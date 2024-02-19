fb3e4bb1ca199ffa55a82bea2c33f9c5d0a908c3
Ryan Zayne <phoenixastro97@gmail.com>
2024-02-19 23:42:02 +0100

    pengs update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,27 @@
 import type { WithPageLayout } from "@/interfaces";
 import { BaseLayout } from "@/layouts";
+import { callApi } from "@/lib";
 import { useSession } from "@/store";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const Home: WithPageLayout = () => {
-	const { user } = useSession((state) => state);
+	const { user, clearSession } = useSession((state) => state);
+	const router = useRouter();
 
-	console.log(user);
+	const signOut = async () => {
+		const { data, error } = await callApi("/auth/signout");
+
+		if (data) {
+			clearSession();
+			void router.push("/signin");
+		}
+	};
+
 	return (
 		<BaseLayout>
 			<Link href="/test"> Go to test </Link>
+			<button onClick={() => void signOut()}>Logout</button>
 			<h1>WELCOME TO ABEG HELP!!</h1>
 		</BaseLayout>
 	);
