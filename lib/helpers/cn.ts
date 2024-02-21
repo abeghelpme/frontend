02ef448115114fa61757b23f6ae6a5b41e431140
Ryan Zayne <phoenixastro97@gmail.com>
2024-02-21 02:40:55 +0100

    chore: added error toasts to a few required areas

--- lib/helpers/cn.ts
+++ lib/helpers/cn.ts
@@ -1,16 +1,5 @@
-import twConfig from "@/tailwind.config";
 import clsx, { type ClassValue } from "clsx";
-import { extendTailwindMerge, twMerge } from "tailwind-merge";
-
-// const themeObject = twConfig.theme.extend;
-
-// const customTwMerge = extendTailwindMerge({
-// 	extend: {
-// 		theme: {
-// 			spacing: Object.keys(themeObject.spacing),
-// 		},
-// 	},
-// });
+import { twMerge } from "tailwind-merge";
 
 const cn = (...classNames: ClassValue[]): string => twMerge(clsx(classNames));
 
