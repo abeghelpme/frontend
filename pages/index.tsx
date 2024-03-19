5616c7923f4bd3818c82c7fa214bb8d7615941c0
Akinniyi Ezekiel Wonderful <wonderfulakinniyi@gmail.com>
2024-03-19 10:18:32 +0100

    added image for campaigns page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
-import { Hero } from "@/components/common/landingPage";
-import { Button, ProgressBar } from "@/components/ui";
+import { FAQ, Hero, UrgentFundraisers } from "@/components/common/landingPage";
+import { Button } from "@/components/ui";
 import { BaseLayout } from "@/layouts";
 import {
 	avatar1,
@@ -14,8 +14,6 @@ import {
 	envato,
 	globalCommunity,
 	google,
-	happyPeople,
-	hero,
 	joinnUs,
 	netflix,
 	senville,
@@ -25,171 +23,6 @@ import {
 } from "@/public/assets/images/landing-page";
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
-
-// FAQ data (questions and answers)
-const faqData = [
-	{
-		question: "How does AbegHelp.me work?",
-		answer: (
-			<>
-				<p>Here&apos;s how fundraising on AbegHelp.me works:</p>
-				<p>
-					To get started, just tap the &apos;Start fundraiser&apos; button.
-					After a few questions to help set up your fundraiser, you&apos;ll then
-					write your fundraising story. Next, you&apos;ll customize your
-					fundraiser, adding details to let potential donors understand your
-					cause.
-				</p>
-				<p>
-					Once your fundraiser is live, you can always add additional photos and
-					videos or lower or raise your goal anytime. The best way to raise
-					funds is by sharing your fundraiser. Call family, text friends,
-					message coworkers, or post your fundraiser link on social media to
-					help you reach your fundraising goal.
-				</p>
-				<p>
-					If you need us, we&apos;ll be there every step of the way with
-					fundraising tips and support. Ready to get started? Start a
-					AbegHelp.me fundraiser today!
-				</p>
-			</>
-		),
-	},
-	{
-		question: "What can I raise money for?",
-		answer: (
-			<>
-				<p>
-					We see people use AbegHelp.me to raise money for themselves, friends
-					and family, or even complete strangers in random acts of kindness.
-					People raise money for just about everything, including medical
-					expenses, education costs, volunteer programs, youth sports, funerals
-					& memorials, and even animals & pets.
-				</p>
-
-				<p>
-					We&apos;re always amazed at the ways people use AbegHelp.me to raise
-					money. Check out some recent success stories, or see how AbegHelp.me
-					works.
-				</p>
-			</>
-		),
-	},
-	{
-		question: "How does AbegHelp.me keep fundraisers safe?",
-		answer: (
-			<>
-				<p>
-					AbegHelp.me features the very best in secure payment encryption
-					technology. Your donors&apos; online payments are safe, and your money
-					is stored securely until you&apos;re ready to request a withdrawal via
-					electronic bank transfer.
-				</p>
-			</>
-		),
-	},
-	{
-		question: "Can a friend withdraw the money I raise for them?",
-		answer: (
-			<>
-				<p>
-					Yes. AbegHelp.me makes it easy for another friend or family member to
-					securely access the funds you have raised. Through AbegHelp.me, they
-					will receive direct access to the money you have raised. Please note:
-					You will not be able to enter their bank information during the
-					withdrawal process; they will need to do this themselves.
-				</p>
-			</>
-		),
-	},
-	{
-		question: "Are there any deadlines or time limits?",
-		answer: (
-			<>
-				<p>
-					While there are important withdrawals deadlines you&apos;ll need to
-					observe to avoid donations being refunded to donors, your fundraiser
-					will remain live until you choose to turn off donations or remove the
-					fundraiser altogether. Most organizers leave their fundraisers active
-					indefinitely to refer back to the kind comments and support they
-					received.
-				</p>
-			</>
-		),
-	},
-	{
-		question: "What if I do not reach my goal?",
-		answer: (
-			<>
-				<p>
-					No problem. Reaching your goal is not required. With AbegHelp.me, you
-					keep each and every donation you receive. Your fundraiser will be able
-					to accept donations even after your goal is reached. Once the goal is
-					reached, the progress meter on your fundraiser will show that you have
-					received more than your goal amount. If you&apos;d like to continue
-					raising money, you can keep your fundraiser running for as long as
-					you&apos;d like.
-				</p>
-			</>
-		),
-	},
-	{
-		question: "How do I get donations?",
-		answer: (
-			<>
-				<p>
-					AbegHelp.me provides an easy way to raise money from your friends,
-					family, and online community. Our platform makes it simple to share
-					your fundraiser in a variety of ways to bring in donations, track your
-					progress, and post updates to engage your community.
-				</p>
-			</>
-		),
-	},
-	{
-		question: "Is my country supported?",
-		answer: (
-			<>
-				<p>
-					We are currently supporting 17 African countries. We are working on
-					expanding our support to more countries.
-				</p>
-			</>
-		),
-	},
-];
-
-const urgentFundraisers = [
-	{
-		image: happyPeople,
-		title: "Bringing Dental Care to Undeserved Communities",
-		name: "Locs Designer",
-		proffession: "Health and Welness",
-		amountRaised: "$2,000,000",
-	},
-	{
-		image: happyPeople,
-		title: "Bringing Dental Care to Undeserved Communities",
-		name: "Locs Designer",
-		proffession: "Health and Welness",
-		amountRaised: "$2,000,000",
-	},
-	{
-		image: happyPeople,
-		title: "Bringing Dental Care to Undeserved Communities",
-		name: "Locs Designer",
-		proffession: "Health and Welness",
-		amountRaised: "$2,000,000",
-	},
-	{
-		image: happyPeople,
-		title: "Bringing Dental Care to Undeserved Communities",
-		name: "Locs Designer",
-		proffession: "Health and Welness",
-		amountRaised: "$2,000,000",
-	},
-];
 
 const testimonials = [
 	{
@@ -241,17 +74,6 @@ const campaignCard = [
 const trustedPioneers = [timeCamp, senville, google, dropbox, netflix, envato]; // Trusted Pioneers images
 
 const HomePage = () => {
-	// State to manage the visibility of each question's answer
-	const [expanded, setExpanded] = useState<boolean[]>([]);
-
-	const toggleAnswer = (index: number) => {
-		setExpanded((prevExpanded) => {
-			const newExpanded = [...prevExpanded];
-			newExpanded[index] = !newExpanded[index]; // Toggle the visibility of the answer when the '+' or '-' icon is clicked
-			return newExpanded;
-		});
-	};
-
 	return (
 		<BaseLayout>
 			<Hero
@@ -458,53 +280,7 @@ const HomePage = () => {
 					</div>
 				</div>
 
-				<div className="py-20">
-					<div className="md:items-center md:justify-between lg:flex">
-						<div className="mb-5 space-y-5">
-							<h4 className="text-xl text-gray-500">Donate today</h4>
-							<h1 className="text-4xl font-bold md:text-5xl">
-								Urgent Fundraisers
-							</h1>
-							<p className="text-xl text-gray-500">
-								Join our community of change-makers and make an impact today
-							</p>
-						</div>
-						<Button className="border border-gray-500 font-semibold text-gray-500 outline-none">
-							<Link href="/c/create">Explore campaigns</Link>
-						</Button>
-					</div>
-
-					<div className="mt-14 gap-5 md:flex md:overflow-x-auto md:hide-scrollbar">
-						{urgentFundraisers.map((item: any, index) => (
-							<div
-								key={index}
-								className="mb-5 flex-shrink-0 flex-grow-0 space-y-4 md:mb-0 md:w-[45%] lg:w-[30%]"
-							>
-								<Image
-									src={item.image}
-									alt={item.name}
-									priority
-									width={400}
-									height={400}
-									className="h-60 w-full rounded-md object-cover"
-								/>
-								<h3 className="text-md text-base font-semibold xl:pr-10 xl:text-lg">
-									{item.title}
-								</h3>
-								<p className="text-xs">
-									By: {item.name} - {item.proffession}
-								</p>
-								<ProgressBar
-									value={70}
-									className="rounded-full progress-unfilled:h-1 progress-unfilled:bg-teal-400 progress-filled:bg-abeg-primary"
-								/>
-								<p className="pt-2 text-xs text-abeg-primary">
-									{item.amountRaised} raised
-								</p>
-							</div>
-						))}
-					</div>
-				</div>
+				<UrgentFundraisers padding="py-20" />
 
 				<div>
 					<h1 className="text-center text-4xl font-bold md:px-32 md:text-5xl">
@@ -517,34 +293,7 @@ const HomePage = () => {
 					</div>
 				</div>
 
-				<div className="mt-10">
-					<p className="text-xl text-gray-500">Wanna know more?</p>
-					<h1 className="mb-10 mt-5 text-4xl font-bold md:mb-20 md:text-5xl">
-						Frequently asked questions
-					</h1>
-					{faqData.map((item, index) => (
-						<div key={index} className="mb-4 space-y-2 bg-white p-4">
-							<div className="flex items-center justify-between">
-								<h3
-									className="cursor-pointer text-2xl font-semibold"
-									onClick={() => toggleAnswer(index)}
-								>
-									{item.question}
-								</h3>
-								<span
-									className="mr-2 cursor-pointer text-2xl md:text-3xl"
-									onClick={() => toggleAnswer(index)}
-								>
-									{expanded[index] ? "−" : "+"}
-								</span>
-							</div>
-							{expanded[index] && (
-								<div className="space-y-5 pt-5 text-xl"> {item.answer}</div>
-							)}
-							<div className="mt-2 border-t-2 border-gray-300"></div>
-						</div>
-					))}
-				</div>
+				<FAQ padding="py-10" />
 			</div>
 		</BaseLayout>
 	);
