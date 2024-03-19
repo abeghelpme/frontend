f2364ff299aef8128da0327ae7a6077f36aa0a8d
Akinniyi Ezekiel Wonderful <wonderfulakinniyi@gmail.com>
2024-03-19 10:15:31 +0100

    reusable components

--- components/common/landingPage/index.ts
+++ components/common/landingPage/index.ts
@@ -1,4 +1,4 @@
 export { default as Hero } from "./Hero";
-export { default as SuccessStories } from "./SuccessStories";
 export { default as CampaignCategories } from "./CampaignCategories";
-export { default as HowItWorks } from "./HowItWorks";
+export { default as FAQ } from "./FAQ";
+export { default as UrgentFundraisers } from "./UrgentFundraisers";
