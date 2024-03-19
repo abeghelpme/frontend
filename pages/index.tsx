64a3b1ba1c43311032086cd359adee37379ba3b4
masako29 <49831574+1finedev@users.noreply.github.com>
2024-03-19 12:40:46 +0300

    remove trusted by from homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,6 @@ import {
 	globalCommunity,
 	google,
 	happyPeople,
-	hero,
 	joinnUs,
 	netflix,
 	senville,
@@ -506,7 +505,7 @@ const HomePage = () => {
 					</div>
 				</div>
 
-				<div>
+				{/* <div>
 					<h1 className="text-center text-4xl font-bold md:px-32 md:text-5xl">
 						Trusted By Pioneers
 					</h1>
@@ -515,7 +514,7 @@ const HomePage = () => {
 							<Image key={index} src={item} alt={""} width={150} height={150} />
 						))}
 					</div>
-				</div>
+				</div> */}
 
 				<div className="mt-10">
 					<p className="text-xl text-gray-500">Wanna know more?</p>
