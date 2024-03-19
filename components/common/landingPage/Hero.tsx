fa51ddf0d81f023c99d2f81db83913fb4a8bf3fc
Akinniyi Ezekiel Wonderful <96802687+Wondersmasher@users.noreply.github.com>
2024-03-19 01:13:33 +0100

    Reusable components (#222)
    * geoJson data for the world map
    
    * fixed path typo error
    
    * Added exports in index files
    
    * components for the campaign-analytics page
    
    * Fixed conflicts
    
    * renamed features.json to worldmap.json
    
    * Fixed naming conflicts of Shared, ApiResponse, FormInputs
    
    * removed import React,
    
    * arranged index.js
    
    * little changes
    
    * Delete package-lock.json
    
    * Made markers on the map to be dynamic based on the countries passed as props.
    
    * Added arbitrary values to the tailwind config and adjusted them where they are used
    
    * installed sonner
    
    * fixed border width that was removed in tailwind.config
    
    * Added apex charts
    
    * exported necessary components
    
    * added some svgs
    
    * cleanup
    
    * new component for campaign analytics page
    
    * updates:{
    removed scroll bar, fixed 2 import issues
    }
    
    * Removed duplicate lines of same code
    
    * added exports for svg
    
    * added more svg components
    
    * made hero section reusable
    
    * new components for campaign page
    
    * campaign page
    
    * hero image and its export
    
    ---------
    
    Co-authored-by: Bayode Emmanuel <obcbeats@gmail.com>

---
+++ components/common/landingPage/Hero.tsx
@@ -0,0 +1,56 @@
+import { Button } from "@/components/ui";
+import { hero } from "@/public/assets/images/landing-page";
+import Image, { type StaticImageData } from "next/image";
+import Link from "next/link";
+
+type Button = {
+	href: String;
+	text: String;
+};
+type Props = {
+	h1Tag: String;
+	pTag: String;
+	button1: Button;
+	button2?: Button;
+	imageSrc: StaticImageData;
+};
+const Hero = ({ h1Tag, pTag, button1, button2, imageSrc }: Props) => {
+	return (
+		<div
+			className="flex items-center md:py-10 lg:h-screen"
+			style={{
+				backgroundImage: `url(/assets/images/landing-page/background.png)`,
+				backgroundSize: "cover",
+				backgroundPosition: "center",
+			}}
+		>
+			<div className="justify-center px-5 text-gray-50 md:items-center md:justify-between md:px-20 lg:flex lg:gap-20">
+				<div className="mt-5 space-y-5 lg:mt-0">
+					<h1 className="pr-5 text-5xl font-bold md:pr-0 md:text-6xl">
+						{h1Tag}
+					</h1>
+					<p className="pr-10 text-lg text-gray-50 md:pr-5">{pTag} </p>
+					<div className="flex space-x-10 py-5 md:pt-20">
+						<Button className="text-md bg-gray-50 font-semibold text-abeg-primary outline-none">
+							<Link href={`${button1.href}`}>{button1.text}</Link>
+						</Button>
+						{button2 && (
+							<Button className="text-md border border-gray-200 bg-transparent font-semibold text-gray-100 outline-none">
+								<Link href={`${button2.href}`}>{button2.text}</Link>
+							</Button>
+						)}
+					</div>
+				</div>
+				<Image
+					src={imageSrc}
+					alt="hero image"
+					width={300}
+					height={300}
+					priority={true}
+					className="ml-[-1.5rem] flex w-full justify-center py-10 md:justify-start lg:py-0"
+				/>
+			</div>
+		</div>
+	);
+};
+export default Hero;
---
+++ components/common/landingPage/SuccessStories.tsx
@@ -0,0 +1,14 @@
+const SuccessStories = () => {
+	return (
+		<div className="flex flex-col gap-8">
+			<h1 className="text-4xl font-bold md:text-5xl text-center">
+				The Success stories of the <br />
+				benefactors speak for themselves
+			</h1>
+			<div className="flex gap-8 ">
+				<div className="flex gap-4 justify-between "></div>
+			</div>
+		</div>
+	);
+};
+export default SuccessStories;
