dc8f8dce730ef3ada7aaa7fb7049873d91b02adb
Ryan Zayne <phoenixastro97@gmail.com>
2024-04-02 13:23:10 +0100

    Refactor carouselStore creation and remove unused file

--- components/ui/carousel/carouselStoreContext.tsx
+++ components/ui/carousel/carouselStoreContext.tsx
@@ -1,7 +1,8 @@
 import { createCustomContext } from "@/lib/hooks";
+import type { PrettyOmit } from "@/lib/type-helpers";
 import type { SelectorFn } from "@/store";
 import { useEffect, useState } from "react";
-import { useStore } from "zustand";
+import { createStore, useStore } from "zustand";
 import { useShallow } from "zustand/react/shallow";
 import type {
 	CarouselProviderProps,
@@ -9,13 +10,59 @@ import type {
 	CarouselStoreApi,
 	ImagesType,
 } from "./carousel.types";
-import { createCarouselStore } from "./createCarouselStore";
 
 const [Provider, useCarouselContext] = createCustomContext<CarouselStoreApi>({
 	name: "CarouselStoreContext",
 	hookName: "useCarouselStore",
 	providerName: "CarouselContextProvider",
 });
+
+// CarouselStore Creation
+const createCarouselStore = <TImages extends ImagesType>(
+	storeValues: PrettyOmit<CarouselProviderProps<TImages>, "children">
+) => {
+	const { images, onSlideBtnClick } = storeValues;
+
+	const carouselStore = createStore<CarouselStore<TImages>>((set, get) => ({
+		images,
+		currentSlide: 0,
+		maxSlide: images.length - 1,
+
+		actions: {
+			goToSlide: (newValue) => {
+				onSlideBtnClick?.();
+
+				set({ currentSlide: newValue });
+			},
+
+			nextSlide: () => {
+				const { currentSlide, maxSlide } = get();
+				const { goToSlide } = get().actions;
+
+				if (currentSlide === maxSlide) {
+					goToSlide(0);
+					return;
+				}
+
+				goToSlide(currentSlide + 1);
+			},
+
+			previousSlide: () => {
+				const { currentSlide, maxSlide } = get();
+				const { goToSlide } = get().actions;
+
+				if (currentSlide === 0) {
+					goToSlide(maxSlide);
+					return;
+				}
+
+				goToSlide(currentSlide - 1);
+			},
+		},
+	}));
+
+	return carouselStore;
+};
 
 // Provider Component
 export function CarouselContextProvider<
