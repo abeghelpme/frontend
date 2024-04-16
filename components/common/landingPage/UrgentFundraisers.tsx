cbaef2db136e9bb1235e9368e020b7f312b641c6
Akinniyi Ezekiel Wonderful <wonderfulakinniyi@gmail.com>
2024-04-16 15:04:03 +0100

    using new design for new index-page

--- components/common/landingPage/UrgentFundraisers.tsx
+++ components/common/landingPage/UrgentFundraisers.tsx
@@ -4,6 +4,7 @@ import { cn } from "@/lib";
 import Link from "next/link";
 import { CampaignCardList } from "../CampaignCard";
 import Heading from "../Heading";
+import { ArrowUpRight } from "../svg";
 
 type UrgentFundraiserProps = {
 	featuredCampaigns: Campaign[];
@@ -20,25 +21,28 @@ function UrgentFundraisers(props: UrgentFundraiserProps) {
 	return (
 		<section className={cn("w-full", className)}>
 			<div className="px-5 md:px-[80px] lg:flex lg:flex-row lg:items-center lg:justify-between">
-				<div className="space-y-2.5">
-					<Heading as="h3" className="text-xl font-normal">
-						Donate Today
-					</Heading>
-
-					<Heading as="h3" className="text-[40px] lg:text-5xl">
-						Urgent Fundraiser
-					</Heading>
-
-					<p className="text-pretty text-xl lg:text-2xl">
-						Join our community of change makers and make an impact today
-					</p>
+				<div className="flex gap-3 items-center">
+					<div className="w-2 bg-abeg-primary rounded-md h-20"></div>
+					<div className="flex flex-col gap-2">
+						<p className="text-xl">Donate today</p>
+						<h3 className="text-2xl md:text-3xl font-bold">
+							Urgent fundraiser
+						</h3>
+					</div>
 				</div>
-				<Button
-					className="mt-4 flex max-w-[185px] shrink-0 justify-center border border-placeholder text-base font-extrabold text-placeholder"
-					asChild
-				>
-					<Link href="/explore">Explore campaigns</Link>
-				</Button>
+				{/* <div className='space-y-2.5'>
+          <Heading as='h3' className='text-xl font-normal'>
+            Donate Today
+          </Heading>
+
+          <Heading as='h3' className='text-[40px] lg:text-5xl'>
+            Urgent Fundraiser
+          </Heading>
+
+          <p className='text-pretty text-xl lg:text-2xl'>
+            Join our community of change makers and make an impact today
+          </p>
+        </div> */}
 			</div>
 
 			<CampaignCardList
@@ -46,6 +50,19 @@ function UrgentFundraisers(props: UrgentFundraiserProps) {
 				cardDetailsArray={featuredCampaigns}
 				classNames={{ base: "mt-10 lg:mt-14 pl-5 md:pl-[80px]" }}
 			/>
+			<div className="px-5 md:px-[80px] pt-5">
+				<Button
+					className="mt-4 flex max-w-[210px] shrink-0 justify-center border border-abeg-primary text-base text-abeg-primary whitespace-nowrap"
+					asChild
+				>
+					<Link href="/explore" className="flex gap-2">
+						<span>Explore campaigns</span>
+						<div className="inline-block text-abeg-primary">
+							<ArrowUpRight fill="#008080" />
+						</div>
+					</Link>
+				</Button>
+			</div>
 		</section>
 	);
 }
