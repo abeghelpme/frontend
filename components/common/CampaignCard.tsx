382a2ff0fee22405cd6a5f8e14ddd41deba8f57e
Ryan Zayne <phoenixastro97@gmail.com>
2024-05-25 13:51:57 +0100

    Extra bug fixes

--- components/common/CampaignCard.tsx
+++ components/common/CampaignCard.tsx
@@ -226,11 +226,7 @@ export function CampaignCardList(props: CardListProps) {
 	const { dragScrollProps, dragContainerClasses, dragItemClasses } =
 		useDragScroll<HTMLUListElement>();
 
-	if (!cardDetailsArray) {
-		return null;
-	}
-
-	if (cardDetailsArray.length === 0) {
+	if (!cardDetailsArray || cardDetailsArray.length === 0) {
 		return null;
 	}
 
